3d11edd32265834c22a9a3ea7981d7b78ff475f4
makiavel77 <ale.davola@outlook.com>
2022-04-12 12:41:07 +0000

    Added s3 e6  bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,11 @@
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.0.1"
-}
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+} 
