cf3a37c615282d1de6be3270b9368f2e21f10bb6
makiavel77 <ale.davola@outlook.com>
2022-04-12 13:00:11 +0000

    Added s3 e10  bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "alex-s3-bucket"
-  bucket_prefix = "this-is-only-a-test-bucket-delete-me-123"
+  bucket_prefix = "this-is-only-a-test-bucket-3"
   acl    = "private"
   versioning = {
     enabled = true
